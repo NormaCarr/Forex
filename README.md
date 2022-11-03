8de707f9be5cdb69758cbb2c7484166f26fc8617
NormaCarr <77812230+NormaCarr@users.noreply.github.com>
2022-11-02 20:27:21 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 "# Forex" 
-
-To get this application running, make sure you do the following in the Terminal:
-  1. python3 -m venv venv
-  2. source venv/bin/activate
-  3. pip install Flask
-  4. pip install forex_python
-  5. pip install flask_debugtoolbar
-  6. flask run
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from typing import ValuesView
+from dotenv import load_dotenv
+import os
 from flask import Flask,flash,request,render_template, redirect, jsonify, flash, session
 from forex_python.converter import CurrencyRates
 from flask.helpers import url_for
@@ -6,10 +7,11 @@ from flask_debugtoolbar import DebugToolbarExtension
 from convert import currency
 currency_calc = currency()
 currencyR = CurrencyRates()
-
-RESPONSES_KEY = "responses"
+load_dotenv()
+RESPONSES_KEY = os.getenv("RESPONSES_KEY")
+SECRET_KEY=os.getenv("SECRET_KEY")
 app = Flask(__name__)
-app.config['SECRET_KEY']="super-secret"
+app.config['SECRET_KEY']=SECRET_KEY
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
@@ -24,22 +26,24 @@ def display_form():
 @app.route("/getarguments")
 def form_arguments():
     """Request the arguments from the form"""
-    convFrom=request.args.get("convFrom").upper()
-    convTo=request.args.get("convTo").upper()
-    amount=request.args.get("amount")
-    valCurrency=(convFrom,convTo,amount)
+    try:
+       convFrom=request.args.get("convFrom").upper()
+       convTo=request.args.get("convTo").upper()
+       amount=request.args.get("amount")
+       valCurrency=(convFrom,convTo,amount)
 
-    """Pass the dictionary to the method convCurrency in the file convert"""
-    verifyvalue=currency_calc.verifyCurrency(valCurrency)
+       """Pass the tupla to the method convCurrency in the file convert"""
+       verifyvalue=currency_calc.verifyCurrency(valCurrency)
    
-    if verifyvalue != "valid_codes":  
-  
-       """If the codes aren't valid display the error msg in the HTML"""
-       return render_template("displayData.html",result=verifyvalue)
-    """Calculate the currency and call to display the result"""
-    converted=currency_calc.convCurrency(valCurrency)
+       if verifyvalue != "valid_codes":  
+         """If the codes aren't valid display the error msg in the HTML"""
+         return render_template("displayData.html",result=verifyvalue)
+       """Calculate the currency and call to display the result"""
+       converted=currency_calc.convCurrency(valCurrency)
+       return render_template("displayData.html",valC=valCurrency,result=converted)
     
-    return render_template("displayData.html",valC=valCurrency,result=converted)
-
+    except:
+        flash("Operation cannot be processed")
+        return redirect("/")   
     
 
